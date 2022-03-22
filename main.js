26bd2b9e71461e554b8a1a1743d75956c7d74303
EntityN3 <102172087+EntityN3@users.noreply.github.com>
2022-03-23 00:54:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -65,12 +65,12 @@ function start(client) {
                 await client.sendText(message.from, "Hanya bisa diakses digroup & jadikan bot ini sebagai admin digroup!");
                 return;
             }
-            if(message.body.split("@").length === 1){
+            if(message.body.split("@").length === 1 || message.body.split("@.length === 2){
                 await client.sendText(message.from, "*Format Salah!*\n\nContoh Penggunaan:\naddUser@nomor");
                 return;
             }
             let sender = message.body.split("@")[1] + "@c.us";
-            let senderNext = message.body.split("@")[1] + "@c.us";
+            let senderNext = message.body.split("@")[2] + "@c.us";
             await client.addParticipant(sender, senderNext);
             return;
         }
